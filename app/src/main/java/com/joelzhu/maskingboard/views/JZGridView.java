efc5bd99986464920e6c499897fbb3a9b30158e3
JoelZhu <joelzhu1992@163.com>
2017-08-07 11:31:53 +0800

    添加嵌套用GridView控件

---
+++ app/src/main/java/com/joelzhu/maskingboard/views/JZGridView.java
@@ -0,0 +1,31 @@
+package com.joelzhu.maskingboard.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * 自定义控件 - 嵌套用GridView
+ *
+ * @author JoelZhu
+ */
+public class JZGridView extends GridView {
+    public JZGridView(Context context) {
+        super(context);
+    }
+
+    public JZGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public JZGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
